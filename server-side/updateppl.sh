efd01bcb09cbef7abbf939bdbff2fe421400bd30
daktak <daktaklakpak@eml.cc>
2016-05-28 19:59:06 +1000

    added remote example

--- server-side/updateppl.sh
+++ server-side/updateppl.sh
@@ -2,6 +2,18 @@
 mate="mac-address"
 daktak="daktak-dns-name"
 m=`/usr/local/bin/checkmac.sh ${mate}`
-r=`/usr/local/bin/checkmac.sh ${daktak}`
-echo "{\"D\":\"${r}\", \"M\":\"${m}\"}" > /tmp/ppl.json
+d=`/usr/local/bin/checkmac.sh ${daktak}`
+json="{\"D\":\"${d}\", \"M\":\"${m}\""
+#you can also include remote people
+#assuming remote-server has a cronned script updating file "user"
+#u=`ssh user@remote-server "cat user"`
+#if succeed
+#if [ $? -eq 0 ]; then
+  #if not null
+#  if [ -z $m ]; then
+#    json="${json}, \"U\":\"${u}\""
+#  fi
+#fi
+echo "${json} }" > /tmp/ppl.json
 mv /tmp/ppl.json /var/www/localhost/htdocs
+
