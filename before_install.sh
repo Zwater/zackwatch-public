f14cf47aadd74959049a027bfd84e466b9de0715
daktak <daktak@domo.ddns.net>
2018-07-28 22:08:44 +1000

    sed

--- before_install.sh
+++ before_install.sh
@@ -20,10 +20,11 @@ if [ ! -d $HOME/pebble-dev/${PEBBLE_SDK} ]; then
   tar -jxf ${PEBBLE_SDK}.tar.bz2 -C ~/pebble-dev/
   # Install the Python library dependencies locally
   cd ~/pebble-dev/${PEBBLE_SDK}
-  ~/pebble-dev/${PEBBLE_SDK}/bin/pebble sdk install https://github.com/aveao/PebbleArchive/raw/master/SDKCores/sdk-core-4.3.tar.bz2
+  sed -i '286s/.*/        return requests.get("{}{}".format(self.DOWNLOAD_SERVER, path), *args, verify=False)/' pebble-tool/pebble_tool/sdk/manager.py
   virtualenv --no-site-packages .env
   source .env/bin/activate
   pip install -r requirements.txt
   deactivate
   pip install -U pyopenssl==0.13.1 pyasn1 ndg-httpsclient
+  ~/pebble-dev/${PEBBLE_SDK}/bin/pebble sdk install https://github.com/aveao/PebbleArchive/raw/master/SDKCores/sdk-core-4.3.tar.bz2
 fi
